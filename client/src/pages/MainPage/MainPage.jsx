1b5518e8a523f8ba4ad828dbbc2d7c30f68ef2f5
Md Arwaz <mdarwaznew2017@gmail.com>
2024-11-29 15:36:39 +0530

    fix bug

--- client/src/pages/MainPage/MainPage.jsx
+++ client/src/pages/MainPage/MainPage.jsx
@@ -3,9 +3,14 @@ import { Navigate, Outlet } from "react-router-dom";
 import Header from "../../Components/Header/Header";
 import Sidebar from "../../Components/Sidebar/Sidebar";
 import { useAuth } from "../../context/authContext.jsx";
+import Preloader from "../../Preloader.jsx";
 
 const MainPage = () => {
-  const { isLoggedIn } = useAuth();
+  const { isLoggedIn, isLoading } = useAuth();
+
+  if (isLoading) {
+    return <Preloader />
+  };
 
   if (!isLoggedIn) {
     return <Navigate to="/login" />;
