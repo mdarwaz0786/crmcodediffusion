5b2daa530ff28488eee2d0fb2602f69cabc7aae7
Md Arwaz <mdarwaznew2017@gmail.com>
2024-11-29 17:35:03 +0530

    fix bug

--- server/src/jobs/projectDeployment/domainExpiryStatusUpdate.js
+++ server/src/jobs/projectDeployment/domainExpiryStatusUpdate.js
@@ -20,7 +20,6 @@ cron.schedule('0 8 * * *', async () => {
     console.log("expiry status update runs");
     const projectDeployments = await ProjectDeployment.find();
 
-
     if (!projectDeployments || projectDeployments.length === 0) {
       console.log("Project deployments not found");
       return;
